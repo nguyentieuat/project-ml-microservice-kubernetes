2c34d7fa1a32adeec94cd4a7fd74f58204c93a08
Ubuntu <ubuntu@ip-172-31-25-247.ec2.internal>
2023-05-09 12:49:42 +0000

    update config

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath="nguyentieuat/luannt19uda:v1.0.0"
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run luannt19uda \
+        --image=nguyentieuat/luannt19uda:v1.0.0 \
+        --port=80 \
+        --labels app=luannt19uda
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward luannt19uda 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath="nguyentieuat/luannt19uda:v1.0.0"
 # Step 2:  
 # Authenticate & tag
+docker login
+docker tag luannt19uda $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
